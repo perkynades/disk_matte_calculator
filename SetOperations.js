a68ce230633c58b90cd8906ce503954d00f7afc2
Steffen Holanger <holanger.steffen@gmail.com>
2019-09-03 10:28:47 +0200

    Fixing eslinting errors

--- SetOperations.js
+++ SetOperations.js
@@ -1,3 +1,7 @@
+/**
+ * Does opreations on sets. Uses Bit strings for most operations.
+ * Also parses to and from array and bitstring
+ */
 class SetOperations {
     /**
      * Makes a bitstring of a set. SetA is the template for the bitstring
@@ -8,14 +12,15 @@ class SetOperations {
      *      returnValue = "0, 1, 0, 1, 1"
      * @param {*} setUnivarsal The initial set
      * @param {*} setA The set to be compared
+     * @return {string} outstrings
      */
     static makeBitString(setUnivarsal, setA) {
         // initializing the output string
-        let outputString = new String();
+        let outputString = '';
 
         // creating a temporary array to hold all the bit string values
-        let tempArray = [];
-        // Filling output string with 0 for every element in setUniversal 
+        const tempArray = [];
+        // Filling output string with 0 for every element in setUniversal
         setUnivarsal.forEach(() => {
             tempArray.push('0');
         });
@@ -27,56 +32,68 @@ class SetOperations {
         // looping through all values in arraySetA
         for (let i = 0; i < arraySetA.length; i++) {
             // Fetching the current element in Set A
-            let elementInSetA = arraySetA[i];
+            const elementInSetA = arraySetA[i];
             for (let k = 0; k < arraySetUnivarsal.length; k++) {
                 // Fetcing the current element in Set Universal
-                let elementInSetUniversal = arraySetUnivarsal[k];
+                const elementInSetUniversal = arraySetUnivarsal[k];
 
-                // Checking if the elements are the same. If so wrinting a 1 to 
+                // Checking if the elements are the same. If so wrinting a 1 to
                 if (elementInSetUniversal === elementInSetA) {
-                    tempArray[k] = 1
+                    tempArray[k] = 1;
                 }
             }
         }
 
         // Converting the temporary array to a string
-        tempArray.forEach(value => {
+        tempArray.forEach((value) => {
             outputString += value;
-        })
+        });
 
         return outputString;
-
     }
 
+    /**
+     * Retruns a union between two sets
+     * @param {*} setA
+     * @param {*} setB
+     * @return {Set} outputset
+     */
     static union(setA, setB) {
         const outputSet = new Set();
 
         // adding set A to the union
         setA.forEach((value1) => {
             outputSet.add(value1);
-        })
+        });
 
         // adding all values of set B to the union
         setB.forEach((value1) => {
-            outputSet.add(value1)
-        })
+            outputSet.add(value1);
+        });
 
         return outputSet;
     }
 
+    /**
+     * Finds the intersections between two sets within a universal set
+     * @param {set} setUnivarsal
+     * @param {set} setA
+     * @param {set} setB
+     * @return {String}
+     */
     static intersection(setUnivarsal, setA, setB) {
-        let outputIntersectionArray = [];
-        const setA_BitString = this.makeBitString(setUnivarsal, setA);
-        const setB_BitString = this.makeBitString(setUnivarsal, setB);
+        const outputIntersectionArray = [];
+        const setABitString = this.makeBitString(setUnivarsal, setA);
+        const setBBitString = this.makeBitString(setUnivarsal, setB);
 
-        const setA_BitArray = this.makeArrayFromBitString(setA_BitString);
-        const setB_BitArray = this.makeArrayFromBitString(setB_BitString);
+        const setABitArray = this.makeArrayFromBitString(setABitString);
+        const setBBitArray = this.makeArrayFromBitString(setBBitString);
 
         // Looping through the array
-        for (let i = 0; i < setA_BitArray.length; i++) {
-            console.log(`${setA_BitArray[i]} ${setB_BitArray[i]}`);
+        for (let i = 0; i < setABitArray.length; i++) {
+            console.log(`${setABitArray[i]} ${setBBitArray[i]}`);
             // Checking that both arrays at index i holds an 1.
-            if (setA_BitArray[i] == 1 && setB_BitArray[i] == 1) {
+            if (setABitArray[i] == 1 && setBBitArray[i] == 1) {
                 // writing result to output array
 
                 outputIntersectionArray[i] = 1;
@@ -91,19 +108,20 @@ class SetOperations {
     /**
      * Making an array from a set
      * @param {*} set the set to be converted
-     * @returns {Array} output 
+     * @return {Array} output
      */
     static makeArrayFromSet(set) {
         const output = [];
-        set.forEach(value => {
+        set.forEach((value) => {
             output.push(value);
-        })
-        return output
+        });
+        return output;
     }
 
     /**
      * Takes a bitstring and turns it into an array
-     * @param {*} bitstring 
+     * @param {*} bitstring
+     * @return {array}
      */
     static makeArrayFromBitString(bitstring) {
         return bitstring.split('');
@@ -112,7 +130,7 @@ class SetOperations {
     /**
      * Makes a bitstring from an array
      * @param {*} array
-     * @retruns outputSting
+     * @return {String} outputSting
      */
     static makeBitStingFromArray(array) {
         let ouputString = '';
