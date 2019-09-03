6a933d8ab51bac80b7fb56add65f644a8538d3c9
Sander Hurlen <sanderhur1@gmail.com>
2019-09-03 09:44:30 +0200

    InputParser added

--- InputParser.js
+++ InputParser.js
@@ -1,5 +1,92 @@
+/**
+ * Class used to parse input from user. Acts as a middleware between UI and main application.
+ * This ensures that each Set has the same format.
+ */
 class InputParser {
+    /**
+     * Constructs a parsed input object from the string.
+     * Which automatically sends the input through the middleware.
+     * Lets user use object to get input as a string or array.
+     * @param {string} string the string to parse
+     */
+    constructor() {
 
+    }
+    /**
+     * removes whitespace
+     * @param {*} string the string
+     * @return {*} string the returned string
+     */
+    static removeWhitespace(string) {
+        const regex = /\s+/gm;
+        const newString = string.replace(regex, '');
+        if (newString == null || newString == '') {
+            throw new Error('String has to contain at least one character.');
+        }
+        return newString;
+    }
+
+    /**
+     * Remove all extra commas from string
+     * @param {*} string the string to remove extra commas
+     * @return {*} newString without extra commas
+     */
+    static removeExtraComma(string) {
+        const regex = /([,])\,+/gm;
+        const newString = string.replace(regex, ',');
+        console.log(newString.charAt(newString.length - 1));
+        if (newString == null || newString == '' || newString == ',') {
+            throw new Error('String has to contain at least one element');
+        }
+        return newString;
+    }
+
+    /**
+     * Create a array from a string, seperated by ,
+     * @param {*} string the string to create array from
+     * @return {*} arr array of elements to create set with
+     */
+    static createArrayFromString(string) {
+        const arr = string.split(',');
+
+        if (arr.length < 1) {
+            throw new Error('String has to contain at least one element. Excluding ,');
+        }
+        return arr;
+    }
+
+    /**
+     * Returns a parsed string as a string.
+     * @param {*} string string to parse
+     * @return {*} string the parsed input as a string
+     */
+    static parseToString(string) {
+        let str = '';
+        try {
+            str = this.removeWhitespace(string);
+            str = this.removeExtraComma(str);
+        } catch (err) {
+            throw err.message;
+        }
+        return str;
+    }
+
+    /**
+     * Returns parsed input as an array.
+     * @param {*} string the string to parse
+     * @return {*} array the parsed input as a array
+     */
+    static parseToArray(string) {
+        let arr = [];
+        try {
+            let str = this.removeWhitespace(string);
+            str = this.removeExtraComma(str);
+            arr = this.createArrayFromString(str);
+        } catch (err) {
+            throw err;
+        }
+        return arr;
+    }
 }
 
 export default InputParser;
--- main.js
+++ main.js
@@ -10,18 +10,37 @@ const answerList = document.querySelector('#answers');
 const compress_u = document.querySelector('#compress_u');
 
 compress_u.addEventListener('click', (e) => {
-  e.preventDefault();
-  if (input_u.value === '') {
-    msg.classList.add('error');
-    msg.innerHTML = 'Please enter the U-field';
+    e.preventDefault();
+    if (input_u.value === '') {
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter the U-field';
 
-    setTimeout(() => msg.remove(), 4000);
-  } else {
-    const li = document.createElement('li');
-    li.appendChild(
-        document.createTextNode(`U =  ${input_u.value}`)
-    );
+        setTimeout(() => msg.remove(), 4000);
+    } else {
+        const li = document.createElement('li');
+        li.appendChild(
+            document.createTextNode(`U =  ${input_u.value}`)
+        );
 
-    answerList.appendChild(li);
-  }
+        answerList.appendChild(li);
+    }
 });
+try {
+    const test = '2,3,3,3,,,,,,4,5,6';
+    console.log('Before Parsing: ' + test);
+    console.log('After parsing: ' + InputParser.parseToString(test));
+    console.log('test 2');
+
+    const test2 = '8,,,,,,       ';
+    console.log('Before Parsing: ' + test2);
+    console.log('After parsing: ' + InputParser.parseToString(test2));
+
+    console.log('test 3');
+
+    const test3 = '1,';
+    console.log('Before Parsing: ' + test3);
+    console.log('After parsing: ' + InputParser.parseToArray(test3));
+} catch (err) {
+    console.log(err.message);
+}
+
